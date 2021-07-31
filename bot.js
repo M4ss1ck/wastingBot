924700f9fdd1d61da9e597fd808d1686eb207725
Andy <4ndyraul@gmail.com>
2021-07-31 12:24:26 -0700

    pinga 1

--- bot.js
+++ bot.js
@@ -45,6 +45,15 @@ bot.on("text", (msg) => {
         msg.reply_to_message.from.username
     );
   }
+  if (msg.text == "pinga" || "Pinga" || "penga" || "Penga") {
+    return bot.sendMessage(
+      msg.chat.id,
+      "@" +
+        msg.from.username +
+        " cariñosamente manda pa' la pinga a @" +
+        msg.reply_to_message.from.username
+    );
+  }
 });
 
 bot.start();
