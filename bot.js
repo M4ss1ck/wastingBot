1c7cb0fe13817dffd634468d777814ad3e916045
Andy <4ndyraul@gmail.com>
2021-07-30 20:03:01 -0700

    new bot

--- bot.js
+++ bot.js
@@ -1,5 +1,5 @@
 const TeleBot = require("telebot");
-const bot = new TeleBot("1174711451:AAFX3Wx7K9gA7WuD9HWlQ1DyUM9tBhJR2no");
+const bot = new TeleBot("1712168159:AAFhf_IJmGpoEAIW9ZLGknzKIuOjNbScsNE");
 
 bot.on(["/start", "/hello", "/jelou"], (msg) =>
   msg.reply.text("Ya empezaron a joder...")
@@ -13,6 +13,14 @@ bot.on("/chatid", (msg) =>
 
 bot.on(/^\/say (.+)$/, (msg, props) => {
   const text = props.match[1];
+  console.log(msg);
+  return bot.sendMessage(msg.from.id, text, { replyToMessage: msg.message_id });
+});
+
+bot.on(/^\/s\/(.+)\/(.+)/, (msg, props) => {
+  const oldm = props.match[1];
+  const newm = props.match[2];
+  const text = msg.text.replace(oldm, newm);
   return bot.sendMessage(msg.from.id, text, { replyToMessage: msg.message_id });
 });
 
