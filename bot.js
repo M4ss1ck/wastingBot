f4be5a4596bffe8b6996c80676619d57adf08b91
Andy <4ndyraul@gmail.com>
2021-07-31 11:39:34 -0700

    nudes 3

--- bot.js
+++ bot.js
@@ -39,7 +39,7 @@ bot.on(["nude", "Nude", "Nudes", "nudes"], (msg) => {
   const agressor = msg.from.username;
   const text = agressor + " le envía su colección de nudes a " + victim;
   console.log(text);
-  return bot.sendMessage(msg.chat.id, text);
+  return msg.reply.text(text);
 });
 
 bot.start();
